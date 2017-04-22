b87d563bf03e66ffa4f69eaeec9850726a4f5f3c
Bert <zedtrebla@aol.com>
2017-04-22 17:23:15 -0500

    server.js update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app){
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '/../public/home.html'));
 })
-// sends user to survey HTML
+
 app.get('/survey', function(req, res){
   res.sendFile(path.join(__dirname, '/../public/survey.html'));
 })
